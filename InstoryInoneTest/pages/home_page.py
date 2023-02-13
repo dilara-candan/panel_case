6b0a8b12790cbb8d650df9a8e71ed06e8158d13a
dilara-candan <102910747+dilara-candan@users.noreply.github.com>
2023-02-13 23:52:25 +0300

    Update home_page.py

--- InstoryInoneTest/pages/home_page.py
+++ InstoryInoneTest/pages/home_page.py
@@ -3,9 +3,13 @@ from pages.base_page import BasePage
 
 
 class HomePage(BasePage):
-    EXPERIENCE_GROUP = (By.XPATH, '//*[@id="page"]/div/div[2]/div[1]/div/div/ul/li[3]')
-    OPTIMIZE = (By.XPATH, '//*[@id="page"]/div/div[2]/div[1]/div/div/ul/li[3]/div/ul/li[1]')
-    INSTORY = (By.XPATH, '//*[@id="page"]/div/div[2]/div[1]/div/div/ul/li[3]/div/ul/li[1]/ul/li[2]')
+    EXPERIENCE_GROUP_SELECTOR = '.in-sidebar-wrapper__groups:nth-child(3)'
+    OPTIMIZE_SELECTOR = EXPERIENCE_GROUP_SELECTOR + ' .in-sidebar-wrapper__opened ul li:nth-child(1)'
+    INSTORY_SELECTOR = OPTIMIZE_SELECTOR + ' ul li:nth-child(2)'
+
+    EXPERIENCE_GROUP = (By.CSS_SELECTOR, EXPERIENCE_GROUP_SELECTOR)
+    OPTIMIZE = (By.CSS_SELECTOR, OPTIMIZE_SELECTOR)
+    INSTORY = (By.CSS_SELECTOR, OPTIMIZE_SELECTOR)
 
     def select_web_instory_product(self):
         self.hover_and_click_element(*self.EXPERIENCE_GROUP)
